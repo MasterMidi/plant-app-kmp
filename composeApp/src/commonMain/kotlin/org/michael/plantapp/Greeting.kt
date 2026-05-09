dbd1f23b5cf0897a07ea4c7e64a6c98c32ad9366
Michael Andreas Graversen <home@michael-graversen.dk>
2026-05-09 20:13:00 +0200

    chore(compose): remove template scaffolding

--- composeApp/src/commonMain/kotlin/org/michael/plantapp/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package org.michael.plantapp
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/org/michael/plantapp/Platform.kt
+++
@@ -1,7 +0,0 @@
-package org.michael.plantapp
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
